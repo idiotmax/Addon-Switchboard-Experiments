f8e5f70f96771f154091acb3ceb4ddf8f5860712
Sebastian Kaspari <s.kaspari@gmail.com>
2016-03-18 13:32:52 +0100

    Show all experiments - Fixes #1.
    Fetch list of experiments from server and show all experiments.

--- bootstrap.js
+++ bootstrap.js
@@ -13,6 +13,7 @@ XPCOMUtils.defineLazyModuleGetter(this, "Log", "resource://gre/modules/AndroidLo
 
 const PANEL_ID = "switchboard.experiments.panel@androidzeitgeist.com";
 const DATASET_ID = "switchboard.experiments.dataset@androidzeitgeist.com";
+const EXPERIMENTS_CONFIGURATION = "https://raw.githubusercontent.com/mozilla-services/switchboard-experiments/master/experiments.json";
 
 function log(msg) {
   Log.d("expt-addon", msg);
@@ -35,6 +36,7 @@ function startup(data, reason) {
     case ADDON_DOWNGRADE:
       Home.panels.update(PANEL_ID);
       log("upgraded/downgraded");
+      _refreshDataset();
       break;
   }
 
@@ -76,26 +78,43 @@ function _optionsCallback() {
   };
 }
 
+/**
+ * Refresh the data set
+ */
 function _refreshDataset() {
   log("_refreshDataset");
 
+  _fetchExperimentsConfiguration();
+}
+
+/**
+ * A new experiment configuration has been downloaded from the server
+ */
+function _onExperimentsConfigurationDownloaded(configuration) {
+  log("_onExperimentsConfigurationDownloaded");
+
   Task.spawn(function() {
-    let experiments = yield _getExperiments();
+    let enabledExperiments = yield _getEnabledExperiments();
     let items = [];
 
-    for (let name of experiments) {
+    for (let name in configuration) {
+      let isInExperiment = enabledExperiments.indexOf(name) != -1;
       items.push({
         url: "https://github.com/mozilla-services/switchboard-experiments",
-        title: name
+        title: name,
+        background_color: isInExperiment ? "#c5e1a5" : "#ef9a9a"
       });
     }
 
     let storage = HomeProvider.getStorage(DATASET_ID);
     yield storage.deleteAll();
     yield storage.save(items);
-  }).then(null, e => Cu.reportError("Error refreshing dataset " + DATASET_ID + ": " + e));;
+  });
 }
 
+/**
+ * Delete all data of this dataset in HomeProvider
+ */
 function _deleteDataset() {
   log("_deleteDataset");
 
@@ -105,12 +124,49 @@ function _deleteDataset() {
   }).then(null, e => Cu.reportError("Error deleting data from HomeProvider: " + e));
 }
 
-function _getExperiments() {
-  log("_getExperiments");
+/**
+ * Get list of locally enabled experiments
+ */
+function _getEnabledExperiments() {
+  log("_getEnabledExperiments");
 
   return Messaging.sendRequestForResult({
     type: "Experiments:GetActive"
   }).then(experiments => {
     return JSON.parse(experiments);
   });
+}
+
+/**
+ * Fetch list of experiments from server configuration
+ */
+function _fetchExperimentsConfiguration() {
+  log("_fetchExperimentsConfiguration");
+
+  let xhr = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
+
+  try {
+    xhr.open("GET", EXPERIMENTS_CONFIGURATION, true);
+  } catch (e) {
+    Cu.reportError("Error opening request: " + e);
+    return;
+  }
+
+  xhr.onerror = function onerror(e) {
+    Cu.reportError("Error making request: " + e.error);
+  };
+
+  xhr.onload = function onload(event) {
+    if (xhr.status === 200) {
+      try {
+        _onExperimentsConfigurationDownloaded(JSON.parse(xhr.responseText));
+      } catch (e) {
+        Cu.reportError("Error parsing request: " + e);
+      }
+    } else {
+      Cu.reportError("Request to " + url + " returned status " + xhr.status);
+    }
+  };
+
+  xhr.send(null);
 }
