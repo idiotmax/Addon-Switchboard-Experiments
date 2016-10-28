4125b976e1e9fe0d5d3e0382cb253aaf76401ddd
Sebastian Kaspari <s.kaspari@gmail.com>
2016-10-28 11:52:25 +0200

    Remove panel code.
    From now on new functionality will be added to about:experiments which is much more flexible
    than the home panels.

--- bootstrap.js
+++ bootstrap.js
@@ -4,15 +4,10 @@ const { classes: Cc, interfaces: Ci, manager: Cm, utils: Cu } = Components;
 
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-Cu.import("resource://gre/modules/Home.jsm");
-Cu.import("resource://gre/modules/HomeProvider.jsm");
 Cu.import("resource://gre/modules/Task.jsm");
 
-XPCOMUtils.defineLazyModuleGetter(this, "Messaging","resource://gre/modules/Messaging.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "Log", "resource://gre/modules/AndroidLog.jsm", "AndroidLog");
 
-const PANEL_ID = "switchboard.experiments.panel@androidzeitgeist.com";
-const DATASET_ID = "switchboard.experiments.dataset@androidzeitgeist.com";
 const EXPERIMENTS_CONFIGURATION = "https://firefox.settings.services.mozilla.com/v1/buckets/fennec/collections/experiments/records";
 
 const contract = "@mozilla.org/network/protocol/about;1?what=experiments";
@@ -44,7 +39,6 @@ let aboutExperiments = {
   }
 };
 
-
 function log(msg) {
   Log.d("expt-addon", msg);
 }
@@ -52,29 +46,6 @@ function log(msg) {
 function startup(data, reason) {
   log("startup: " + reason);
   Cm.QueryInterface(Ci.nsIComponentRegistrar).registerFactory(uuid, description, contract, aboutFactory);
-
-  Home.panels.register(PANEL_ID, _optionsCallback);
-
-  switch(reason) {
-    case ADDON_INSTALL:
-    case ADDON_ENABLE:
-      Home.panels.install(PANEL_ID);
-      _refreshDataset();
-      log("installed/enabled");
-      break;
-
-    case ADDON_UPGRADE:
-    case ADDON_DOWNGRADE:
-      Home.panels.update(PANEL_ID);
-      log("upgraded/downgraded");
-      _refreshDataset();
-      break;
-  }
-
-  // Update data once every hour.
-  HomeProvider.addPeriodicSync(DATASET_ID, 3600, _refreshDataset);
-
-  log("startup finished");
 }
 
 function shutdown(data, reason) {
@@ -82,79 +53,8 @@ function shutdown(data, reason) {
   Cm.QueryInterface(Ci.nsIComponentRegistrar).unregisterFactory(uuid, aboutFactory);
 
   if (reason == ADDON_UNINSTALL || reason == ADDON_DISABLE) {
-    Home.panels.uninstall(PANEL_ID);
-    _deleteDataset();
     _clearOverrides();
   }
-
-  Home.panels.unregister(PANEL_ID);
-}
-
-function install(data, reason) {
-  log("install");
-}
-
-function uninstall(data, reason) {
-  log("uninstall");
-}
-
-function _optionsCallback() {
-  log("_optionsCallback");
-
-  return {
-    title: "Experiments",
-    views: [{
-      type: Home.panels.View.LIST,
-      dataset: DATASET_ID,
-      onrefresh: _refreshDataset
-    }]
-  };
-}
-
-/**
- * Refresh the data set
- */
-function _refreshDataset() {
-  log("_refreshDataset");
-
-  _fetchExperimentsConfiguration(_onExperimentsConfigurationDownloaded);
-}
-
-/**
- * A new experiment configuration has been downloaded from the server
- */
-function _onExperimentsConfigurationDownloaded(configuration) {
-  log("_onExperimentsConfigurationDownloaded");
-
-  Task.spawn(function() {
-    let enabledExperiments = yield _getEnabledExperiments();
-    let items = [];
-
-    configuration.data.forEach(function(experiment) {
-      let isInExperiment = enabledExperiments.indexOf(experiment.name) != -1;
-      items.push({
-        url: "https://github.com/mozilla-services/switchboard-experiments",
-        title: experiment.name,
-        background_color: isInExperiment ? "#c5e1a5" : "#ef9a9a"
-      });
-    });
-
-    let storage = HomeProvider.getStorage(DATASET_ID);
-    yield storage.deleteAll();
-    yield storage.save(items);
-  });
-}
-
-/**
- * Delete all data of this dataset in HomeProvider
- */
-function _deleteDataset() {
-  log("_deleteDataset");
-
-  Task.spawn(function() {
-    let storage = HomeProvider.getStorage(DATASET_ID);
-    yield storage.deleteAll();
-  }).then(null, e => Cu.reportError("Error deleting data from HomeProvider: " + e));
 }
 
 /**
@@ -167,19 +67,6 @@ function _clearOverrides() {
     log("_clearOverridesFromConfiguration");
 
     configuration.data.forEach((experiment) => Experiments.clearOverride(experiment.name));
-  });
-}
-
-/**
- * Get list of locally enabled experiments
- */
-function _getEnabledExperiments() {
-  log("_getEnabledExperiments");
-
-  return Messaging.sendRequestForResult({
-    type: "Experiments:GetActive"
-  }).then(experiments => {
-    return JSON.parse(experiments);
   });
 }
 
